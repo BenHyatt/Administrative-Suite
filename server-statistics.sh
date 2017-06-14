aff6d08fcdf0ee6198819c2f6fd1f4b83cf5dcd3
Ben Underwood <benu423@gmail.com>
2017-06-14 12:41:28 -0400

    Create server-statistics.sh

--- server-statistics.sh
+++ server-statistics.sh
@@ -7,9 +7,7 @@ date=$(date)
 storage=($(df -h|awk 'NR==2'|awk '{print $5}'))
 ram=($(free -h|awk 'NR==2'|awk '{print $4}'))
 users=$(who | wc -l)
-uptime_1=$(uptime|awk '{print $3}')
-uptime=$(uptime|awk '{print $4}')
-uptime=${uptime::-1}
+uptime=$(uptime -p)
 if [ "$users" == "1" ]
 then
         message="is ${red}1${reset}"
@@ -21,6 +19,6 @@ echo "You are using ${red}$storage${reset} of storage."
 echo "You have ${red}$ram${reset} RAM available."
 echo "There $message different SSH connections."
 echo "The CPU temperature is ${red}$temp°C${reset}."
-echo "The server has been up for ${red}$uptime_1 $uptime${reset}."
+echo "The server has been ${red}$uptime${reset}."
 echo -e
 echo "Server-Statistics was produced by ${green}Ben Corp Solutions${reset}"
